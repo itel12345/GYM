7b85163f1e07625dd6179ba83e97edea8f6d93ef
Loop Games <itelemma0@gmail.com>
2025-06-29 03:40:25 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
     const menu = document.getElementById("dropdown-content");
     menu.style.display = menu.style.display === "flex" ? "none" : "flex";
   }
+const searchInput = document.getElementById("searchInput");
+const searchItems = document.querySelectorAll(".search-item");
+
+searchInput.addEventListener("input", function () {
+  const filter = searchInput.value.toLowerCase().trim();
+
+  searchItems.forEach(item => {
+    const text = item.textContent.toLowerCase();
+    if (text.includes(filter)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
